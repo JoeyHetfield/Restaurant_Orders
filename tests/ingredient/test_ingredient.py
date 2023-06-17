a0d566bc8cd106e967eb4eb4438410133c5d775e
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-17 13:47:50 -0300

    teste repr

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -4,3 +4,6 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 def test_ingredient():
     item = Ingredient("bacon")
     assert item.name == "bacon"
+
+    expected_repr = "Ingredient('bacon')"
+    assert repr(item) == expected_repr
