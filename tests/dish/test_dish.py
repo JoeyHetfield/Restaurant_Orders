5cca380aadfda07cf65988e9928e898769ef16f0
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-17 15:34:33 -0300

    iniciando testes de dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -3,4 +3,13 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 # Req 2
 def test_dish():
-    pass
+    food = Dish("hamburguer", 15.0)
+    assert food.name == "hamburguer"
+
+    food2 = Dish("pizza", 27.0)
+    assert food2.name != food.name
+    assert food2 != food
+
+    food3 = Dish("hamburguer", 15.0)
+    assert food3.name == food.name
+    assert food3 == food
