213b7e65f7d9e01cdec3b11ab3ed036fbfad96d7
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-18 11:31:49 -0300

    criando metodo para ler o arquivo

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -7,3 +7,8 @@ class MenuData:
     def __init__(self, source_path: str) -> None:
         self.dishes = set()
         self.ingredients = set()
+        self.source_path = source_path
+
+    def read_file(self):
+        with open(self.source_path, "r") as file:
+            pass
