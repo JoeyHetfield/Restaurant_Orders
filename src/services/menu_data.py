a42ff446ba182c66fca8d03ab4366cb6dd764cd2
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-19 16:48:44 -0300

    menu_data feio

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,5 +1,6 @@
 from src.models.dish import Dish
 from src.models.ingredient import Ingredient
+import csv
 
 
 # Req 3
@@ -8,7 +9,28 @@ class MenuData:
         self.dishes = set()
         self.ingredients = set()
         self.source_path = source_path
+        self.read_file()
 
     def read_file(self):
         with open(self.source_path, "r") as file:
-            pass
+            reader = csv.reader(file)
+            next(reader)
+
+            dishes = {}
+
+            for row in reader:
+                dish_name = row[0]
+                dish_price = float(row[1])
+                ingredient_name = row[2]
+                ingredient_quantity = int(row[3])
+
+                if dish_name not in dishes:
+                    dish = Dish(dish_name, dish_price)
+                    dishes[dish_name] = dish
+                    self.dishes.add(dish)
+                else:
+                    dish = dishes[dish_name]
+
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(ingredient, ingredient_quantity)
+                self.ingredients.add(ingredient)
